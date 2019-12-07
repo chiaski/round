7f71b423e6fe3aa06460cc2665e460df819a90fd
Chia <chiamisola@gmail.com>
2019-12-07 07:43:20 -0500

    Modify server

--- public/assets/js/draw.js
+++ public/assets/js/draw.js
@@ -1,28 +1,20 @@
 let socket
-let color = '#FFF'
+let color = '#ff0000'
 let strokeWidth = 4
 
 var hue;
 
-
 function setup() {
+    hue = 0;
     
 	// Creating canvas
 	const cv = createCanvas(windowWidth, windowHeight)
-	cv.background(10)
-    hue = 0;
+	cv.background(0)
+    
+    colorMode(HSL);
 
 	// Start the socket connection
-    // var socket = io.connect("https://chia.dev/code/allaroundme:3000");
-	socket = io.connect(); //("130.132.173.78:3000");
-
-	// Callback function
-	socket.on('mouse', data => {
-		stroke(data.color)
-		strokeWeight(data.strokeWidth)
-		line(data.x, data.y, data.px, data.py)
-	})
-    
+	socket = io.connect(); 
     
 
 	// Getting our buttons and the holder through the p5.js dom
@@ -32,17 +24,25 @@ function setup() {
 
 	const stroke_width_picker = select('#stroke-width-picker')
 	const stroke_btn = select('#stroke-btn')
-
+    
+	// Callback function
+	socket.on('mouse', data => {
+		stroke(data.color)
+		strokeWeight(data.strokeWidth)
+		line(data.x, data.y, data.px, data.py)
+	})
+    
+    
 	// Adding a mousePressed listener to the button
-	color_btn.mousePressed(() => {
+	/* color_btn.mousePressed(() => {
 		// Checking if the input is a valid hex color
 		if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
 			color = color_picker.value()
 			color_holder.style('background-color', color)
 		}
 		else {console.log('Enter a valid hex value')}
-	})
-
+	}) */
+    
 	// Adding a mousePressed listener to the button
 	stroke_btn.mousePressed(() => {
         
@@ -55,15 +55,25 @@ function setup() {
           noStroke();
           fill(hue, 200, 200);
         
-          ellipse(mouseX, mouseY, 3, 3);
 	})
     
     
 }
 
 function mouseDragged() {
+    
+          if (hue > 360) {
+            hue = 0;
+          } else {
+            hue += 10;
+          }
+          colorMode(HSL, 360);
+          noStroke();
+          fill(hue, 200, 200);
+        
+    
 	// Draw
-	stroke(color)
+	stroke(hue)
 	strokeWeight(strokeWidth)
 	line(mouseX, mouseY, pmouseX, pmouseY)
 
@@ -78,7 +88,7 @@ function sendmouse(x, y, pX, pY) {
 		y: y,
 		px: pX,
 		py: pY,
-		color: color,
+		color: hue,
 		strokeWidth: strokeWidth,
 	}
 
