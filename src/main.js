4fc429d47436993d324e0ab0b21ba7a3e662a8fa
Chia <chiamisola@gmail.com>
2019-12-08 04:50:46 -0500

    Add street view

--- src/main.js
+++ src/main.js
@@ -2,3 +2,23 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 console.log(require('dotenv').config())
+
+
+/*
+function initialize() {
+      var fenway = {lat: 42.345573, lng: -71.098326};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: fenway,
+        zoom: 14
+      });
+      var panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), {
+            position: fenway,
+            pov: {
+              heading: 34,
+              pitch: 10
+            }
+          });
+      map.setStreetView(panorama);
+    }
+    */
